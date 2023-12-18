63ac8b5c372392e0d6cd315565a7324189b573a4
Klevi Merkuri <49405904+kmerkuri@users.noreply.github.com>
2023-12-19 00:38:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-
-import os
+from kubernetes import client, config
 import sys
-import json
 from datetime import datetime, timedelta
-from flask import Flask, request
+from flask import Flask,request, jsonify
 import jira
 from jira.exceptions import JIRAError
+import os
+import base64
+from flask_sslify import SSLify
+from OpenSSL import crypto
 app = Flask(__name__)
+sslify = SSLify(app)
+def get_secret(namespace, secret_name):
+    config.load_incluster_config()
 
+    v1 = client.CoreV1Api()
+
+    try:
+        secret = v1.read_namespaced_secret(name=secret_name, namespace=namespace)
+        return secret
+    except client.rest.ApiException as e:
+        print(f"Error retrieving secret: {e}")
+        return None
 def create_jira_client():
     server = os.environ['JIRA_SERVER']
     username = os.environ['JIRA_USERNAME']
@@ -39,25 +52,34 @@ def create_ticket(client, summary, description, issue_type='Task'):
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    payload = request.get_json()
+    data = request.json
+    certificate_name = data['request']['object']['metadata']['name']
+    expiry_date = data['request']['object']['status']['notAfter']
+    namespace = data['request']['object']['metadata']['namespace']
 
-    # Get the certificate expiration date from the payload
-    expiration_date = datetime.strptime(payload['certificate']['notAfter'], '%Y-%m-%dT%H:%M:%SZ')
+    secret_name = data['request']['object']['spec']['secretName']
+    secret = get_secret(namespace, secret_name)
+    threshold_days = 10
+    if secret is not None:
+        certificate = secret.data['tls.crt']
+        certificate_bytes = base64.b64decode(certificate)
+        x509 = crypto.load_certificate(crypto.FILETYPE_PEM, certificate_bytes)
+        certificate_expiration = x509.get_notAfter().decode('utf-8')
+        expiration_date = datetime.strptime(certificate_expiration, '%Y%m%d%H%M%SZ')
+        threshold_date = datetime.utcnow() + timedelta(days=threshold_days)
 
-    # Compare the expiration date with the current date
-    now = datetime.now()
-    expiration_threshold = timedelta(days=os.environ['CERTIFICATE_EXPIRATION_THRESHOLD'])
-    is_certificate_expiring = (expiration_date - now) <= expiration_threshold
+        if expiration_date <= threshold_date:
+            jira_client = create_jira_client()
+            summary = f"Certificate Expiration: {certificate_name}"
+            description = f"The certificate {certificate_name} will expire on {certificate_expiration}."
+            create_ticket(jira_client, summary, description)
 
-    if is_certificate_expiring:
-        jira_client = create_jira_client()
-        summary = f"Certificate Expiration: {payload['certificate']['commonName']}"
-        description = f"The certificate {payload['certificate']['commonName']} will expire on 
-{expiration_date.strftime('%Y-%m-%d')}."
-        create_ticket(jira_client, summary, description)
+            return jsonify({"apiVersion": "admission.k8s.io/v1","kind": "AdmissionReview","response": {"uid": request.json['request']['uid'],"allowed": True}}), 200
 
-    return "OK", 200
+        return jsonify({"apiVersion": "admission.k8s.io/v1","kind": "AdmissionReview","response": {"uid": request.json['request']['uid'],"allowed": True}}), 200
+
+    return jsonify({"status": "failure", "message": "Certificate is not expired"}), 400
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-
+    app.run(host='0.0.0.0', port=8080,ssl_context=('/app/certs/tls.crt', '/app/certs/tls.key'))
